978bffb60b9cd2996a94bd13ca08200cb61455ec
ramcj7742 <ramcj7742@wrdsb.ca>
2020-10-02 16:25:40 +0000

    save

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Does the hailstone pattern from user number
+ * @author Jaden Rancharan
  */
 public class Main {
 
@@ -9,7 +10,21 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Creates scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //gets user number
+    System.out.println("Please enter a positive integer to see the hailstone sequence.");
+    int num = input.nextInt();
+
+    //hailstone pattern
+    while(num != 1){
+      if(num % 2 == 0){
+        System.out.println(num / 2);
+      }else if(num % 2 != 0){
+        System.out.println(num * 3 + 1);
+      }
+    }
     
   }
 }
